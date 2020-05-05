6b5a940e80801d6137db9e71638e6be19235873f
Justin James <justin.james@yale.edu>
2020-05-05 17:45:56 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0] 
 
 end
 
@@ -81,7 +83,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["Lisp", "C"]
+    }
+    programmer_hash
 end
 
 def changing_alan
@@ -103,6 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +133,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
